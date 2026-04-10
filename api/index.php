fa42f25db23abf6dd88ab31e5738765606a0f9fc
malkKhalid <malbana4@smail.ucas.edu.ps>
2026-04-10 09:28:46 +0300

    Final fix for api/index.php and dependencies

--- api/index.php
+++ api/index.php
@@ -4,14 +4,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+// 1. تعديل مسار صيانة التطبيق
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+// 2. تعديل مسار الـ Composer (vendor)
+require __DIR__ . '/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+// 3. تعديل مسار الـ Bootstrap
+(require_once __DIR__ . '/../bootstrap/app.php')
     ->handleRequest(Request::capture());
