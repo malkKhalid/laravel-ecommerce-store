001825f252915a3aa43c49613dfd9c69630f59f1
malkKhalid <malbana4@smail.ucas.edu.ps>
2026-04-10 16:07:00 +0300

    Fix layout issues by forcing https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
 
     }
 }
